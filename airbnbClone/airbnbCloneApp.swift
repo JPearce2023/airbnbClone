bb2ef32f95cead45f0cd59de2bf9e40e944de9ce
Jeff P <jeff.acdc300@gmail.com>
2024-05-29 18:10:40 -0500

    Initial Commit

---
+++ airbnbClone/airbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbCloneApp.swift
+//  airbnbClone
+//
+//  Created by Jeff Pearce on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct airbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
