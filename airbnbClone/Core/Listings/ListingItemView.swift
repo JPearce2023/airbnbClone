552025c7d4e99bca9ea097b96cf14dce3879dc7d
Jeff P <jeff.acdc300@gmail.com>
2024-05-29 19:11:01 -0500

    initial code

--- airbnbClone/Core/Listings/ListingItemView.swift
+++ airbnbClone/Core/Listings/ListingItemView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct ListingItemView: View {
+    
+    var images = [
+        "image-1",
+        "image-2",
+        "image-3",
+        "image-4",
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 8) {
+            // images
+            TabView() {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+            
+            // listing details
+            HStack (alignment: .top) {
+                // details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+
+                    
+                    HStack (spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                    
+                }
+                
+                Spacer()
+                
+                // rating
+                
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.86")
+                }
+                
+            }
+            
+            .font(.footnote)
+            
+        }
     }
 }
 
 #Preview {
-    ListingView()
+    ListingItemView()
 }
