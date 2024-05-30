a8a3080887e94865c3c745fae1afc28b44e54b30
Jeff P <jeff.acdc300@gmail.com>
2024-05-29 19:07:08 -0500

    Initial Code for AirBnB clone

---
+++ airbnbClone/Core/Listings/ListingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  airbnbClone
+//
+//  Created by Jeff Pearce on 5/29/24.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingView()
+}
